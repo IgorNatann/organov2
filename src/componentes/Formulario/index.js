f137e8429210402d8be2b4f48ac24bc406744886
Igor Natan <igornatan4@gmail.com>
2023-10-16 15:49:16 -0300

    commit componente  time

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css"
   
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        "Devops",
+        "Ux e Design",
+        "Mobile",
+        "Inovação e gestão"
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,6 +20,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome:" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo:" placeholder="Digite o seu cargo" />
                 <CampoTexto label="Imagem:" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Selecione o seu time:" itens={times}/>
             </form>
         </section>
     )
